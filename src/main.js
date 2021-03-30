2562e6d2dc9d42d44b5ca4bd2c8ad130fcc56ecd
yousangJo <jysrmb@gmail.com>
2021-03-30 23:02:49 +0900

    first

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
+import "@babel/polyfill";
+import "mutationobserver-shim";
 import Vue from "vue";
+import "./plugins/fontawesome";
+import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./assets/scss/index.scss";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+    router,
+    store,
+    render: (h) => h(App),
 }).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import getters from "./modules/Getters";
+import actions from "./modules/Actions";
+import mutations from "./modules/Mutations";
 
 Vue.use(Vuex);
 
+const state = {};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+    state,
+    mutations,
+    actions,
+    getters
 });
